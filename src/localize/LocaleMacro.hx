507ee5c7c9184340826d57f7eec2ec8b87f4244e
kevinresol <kevinresol@gmail.com>
2016-06-15 11:46:05 +0800

    Indentaion

--- src/localize/LocaleMacro.hx
+++ src/localize/LocaleMacro.hx
@@ -10,19 +10,19 @@ class LocaleMacro {
         for (field in fields) {
             switch (field.kind) {
                 case FVar(type, _):
-                  // TODO: ensure String type?
-                  field.access = [APublic];
-                  field.kind = FProp('get', 'never', type, null);
-                  fields.push({
-                    access: [AInline],
-                    kind: FFun({
-                      args: [],
-                      expr: macro return this.__catalog.get(new LocaleKey($v{field.name})),
-                      ret: type
-                    }),
-                    name: 'get_' + field.name,
-                    pos: field.pos,
-                  });
+                    // TODO: ensure String type?
+                    field.access = [APublic];
+                    field.kind = FProp('get', 'never', type, null);
+                    fields.push({
+                        access: [AInline],
+                        kind: FFun({
+                            args: [],
+                            expr: macro return this.__catalog.get(new LocaleKey($v{field.name})),
+                            ret: type
+                        }),
+                        name: 'get_' + field.name,
+                        pos: field.pos,
+                    });
                 case FFun(fun) if (fun.expr == null):
                     field.access = [APublic, AInline];
                     if (fun.args.length > 0) {
--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -11,47 +11,41 @@ import Sys.*;
 
 class RunTests extends TestCase {
 
-  static function main() {
-    var runner = new TestRunner();
-    runner.add(new RunTests());
-    exit(runner.run() ? 0 : 500);
-  }
-  
-  function testCatalog() {
-    
-    var catalog = new Catalog([
-        "pony_name" => "A pony",
-        "pony_message" => "Hey, {name}, you just received {count} ponies as a gift!",
-    ]);
+    static function main() {
+        var runner = new TestRunner();
+        runner.add(new RunTests());
+        exit(runner.run() ? 0 : 500);
+    }
 
-    var pony = new Gift("pony_name", "pony_message");
-    assertEquals('A pony', catalog.get(pony.name));
-    assertEquals('Hey, Dan, you just received 5 ponies as a gift!' ,catalog.interpolate(pony.message, {name: "Dan", count: 5}));
-  }
-  
-  function testLocale() {
-    
-    var catalog = new Catalog([
-        "item" => "Apple",
-        "person" => "Dan",
-        "message" => "Hi, {name} ({count} times)!",
-    ]);
-    
-    var locale = new MyLocale(catalog);
-    assertEquals('Hi, Dan (5 times)!', locale.message(locale.person(), 5));
-    assertEquals('Apple', locale.item);
-  }
-  
-  function testMissingCatalogEntry() {
-    
-    var catalog = new Catalog([
-        "message" => "Hi, {name} ({count} times)!",
-    ]);
+    function testCatalog() {
+        var catalog = new Catalog([
+            "pony_name" => "A pony",
+            "pony_message" => "Hey, {name}, you just received {count} ponies as a gift!",
+        ]);
+        var pony = new Gift("pony_name", "pony_message");
+        assertEquals('A pony', catalog.get(pony.name));
+        assertEquals('Hey, Dan, you just received 5 ponies as a gift!' ,catalog.interpolate(pony.message, {name: "Dan", count: 5}));
+    }
 
-    var locale = new MyLocale(catalog);
-    assertEquals('Hi, NOT LOCALIZED: person (5 times)!', locale.message(locale.person(), 5));
-    assertEquals('NOT LOCALIZED: item', locale.item);
-  }
+    function testLocale() {
+        var catalog = new Catalog([
+            "item" => "Apple",
+            "person" => "Dan",
+            "message" => "Hi, {name} ({count} times)!",
+        ]);
+        var locale = new MyLocale(catalog);
+        assertEquals('Hi, Dan (5 times)!', locale.message(locale.person(), 5));
+        assertEquals('Apple', locale.item);
+    }
+
+    function testMissingCatalogEntry() {
+        var catalog = new Catalog([
+            "message" => "Hi, {name} ({count} times)!",
+        ]);
+        var locale = new MyLocale(catalog);
+        assertEquals('Hi, NOT LOCALIZED: person (5 times)!', locale.message(locale.person(), 5));
+        assertEquals('NOT LOCALIZED: item', locale.item);
+    }
   
 }
 class Gift {
